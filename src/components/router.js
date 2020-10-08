009008386ae634f45817ce786760acbccbbcd143
Konrad <konrad.gmur@gmail.com>
2020-10-08 19:37:50 +0200

    Adding router.js to project

---
+++ src/components/router.js
@@ -0,0 +1,13 @@
+import VueRouter from "vue-router";
+
+const Home = { template: "<div>Strona główna</div>" };
+const About = { template: "<div>O nas</div>" };
+const Contact = { template: "<div>Kontakt</div>" };
+
+const router = new router({
+  routes: [
+    { path: "/", component: Home },
+    { path: "/onas", component: About },
+    { path: "/kontakt", component: Contact },
+  ],
+});

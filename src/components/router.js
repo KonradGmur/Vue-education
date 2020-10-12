ac14c31bc6796c3e5e54dc7c5ce8eab78a2dd684
Konrad <konrad.gmur@gmail.com>
2020-10-12 20:41:23 +0200

    Nesting

--- src/components/router.js
+++ src/components/router.js
@@ -3,6 +3,9 @@ import VueRouter from "vue-router";
 const Home = { template: "<div>Strona główna</div>" };
 const About = { template: "<div>O nas</div>" };
 const Contact = { template: "<div>Kontakt</div>" };
+const UserGeneral = {
+  template: "<div>informacje o użytkowniku <router-view/></div>",
+};
 const User = {
   props: ["userId"],
   template: "<div>Id użytkownika: {{userId}}</div>",
@@ -14,6 +17,17 @@ const router = new router({
     { path: "/", component: Home },
     { path: "/onas", component: About },
     { path: "/kontakt", component: Contact },
+    {
+      path: "/uzytkownicy",
+      component: UserGeneral,
+      children: [
+        {
+          path: "profil/:userId",
+          component: User,
+          props: true,
+        },
+      ],
+    },
     { path: "/uzytkownik/:userId", component: User, props: true },
   ],
 });

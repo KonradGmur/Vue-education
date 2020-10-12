ed437d2f0102c2b865c435f74996d3a6f21034d7
Konrad <konrad.gmur@gmail.com>
2020-10-12 20:24:32 +0200

    Vue router dynamic paths

--- src/components/router.js
+++ src/components/router.js
@@ -3,6 +3,9 @@ import VueRouter from "vue-router";
 const Home = { template: "<div>Strona główna</div>" };
 const About = { template: "<div>O nas</div>" };
 const Contact = { template: "<div>Kontakt</div>" };
+const User = {
+  template: "<div>Id użytkownika: {{$route.params.userId}}</div>",
+};
 
 const router = new router({
   mode: "history",
@@ -10,6 +13,7 @@ const router = new router({
     { path: "/", component: Home },
     { path: "/onas", component: About },
     { path: "/kontakt", component: Contact },
+    { path: "/uzytkownik/:userId", component: User },
   ],
 });
 

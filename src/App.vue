22c444d86792cb8d37444c0be21fd49ffa752e2a
Konrad <konrad.gmur@gmail.com>
2020-10-13 20:34:40 +0200

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Witaj w Vue.js</h1>
     <ul>
       <li><router-link to="/">Strona główna</router-link></li>
-      <li><router-link to="/onas">O nas</router-link></li>
+      <li><router-link :to="{ name: 'o nas' }">O nas</router-link></li>
       <li><router-link to="/kontakt">Kontakt</router-link></li>
     </ul>
     <router-view></router-view>

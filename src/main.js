4fa5f2d304817d8f6dce19143631fa40def790d4
Konrad <konrad.gmur@gmail.com>
2020-10-14 19:20:32 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import router from "./components/router";
+import Store from "./store";
 
 Vue.config.productionTip = false;
 
@@ -9,6 +10,7 @@ Vue.use(vueRouter);
 
 new Vue({
   render: (h) => h(App),
+  store,
   router,
   components: { App },
   template: "<App/>",

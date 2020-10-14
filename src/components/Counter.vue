725082f340fac345e8a24d03af4d56623bfbd678
Konrad <konrad.gmur@gmail.com>
2020-10-14 19:15:24 +0200

    Vuex in practice part 2

---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Wartość licznika to: {{ licznik }}</h1>
+    <button @click="zwiekszLicznik">Dodaj 1</button>
+    <button @click="zmniejszLicznik">Odejmij 1</button>
+  </div>
+</template>
+
+<script>
+import store from "../store";
+
+export default {
+  computed: {
+    licznik: () => store.getters.pobierzLicznik,
+  },
+  methods: {
+    dodaj: () => store.dispatch("zwiekszLicznik"),
+    odejmij: () => store.dispatch("zmniejszLicznik"),
+  },
+};
+</script>

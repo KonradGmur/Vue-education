ba809897abde4553967074d9c43bf08a65b33be5
Konrad <konrad.gmur@gmail.com>
2020-10-14 19:21:04 +0200

    Vuex in practice part 3

--- src/components/App2.vue
+++ src/components/App2.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
     <h1>Vuex- zarządzanie stanem aplikacji</h1>
+    <Counter></Counter>
   </div>
 </template>
 <script>
+import Counter from "./Counter";
+
 export default {
   name: "App",
+  components: {
+    Counter,
+  },
 };
 </script>
 
